cc89e236db418235b902e2be9f1f7ef380c05a62
Abhii_7 <129268835+Abhivarun7@users.noreply.github.com>
2025-04-02 18:35:55 +0530

    Delete server directory

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getUserData } from '../controllers/userController.js';
-import userAuth from '../middlewares/userAuth.js';
-const UserRouter = express.Router();
-
-UserRouter.get('/data', userAuth, getUserData);
-
-export default UserRouter;
